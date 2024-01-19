d9ca1cd678514a2c03230396293ee0b6d4522916
Lara, Benjamin R <ben.lara@hashicorp.com>
2024-01-19 12:39:37 -0500

    fix: :bug: must use nonsensitive() to pass sensitive data

--- secrets/outputs.tf
+++ secrets/outputs.tf
@@ -1,9 +1,7 @@
 output "hcp_client_id" {
-  value = data.vault_kv_secret_v2.hcp_creds.data["client_id"]
-  sensitive = true
+  value = nonsensitive(data.vault_kv_secret_v2.hcp_creds.data["client_id"])
 }
 
 output "hcp_client_secret" {
-  value = data.vault_kv_secret_v2.hcp_creds.data["client_secret"]
-  sensitive = true
+  value = nonsensitive(data.vault_kv_secret_v2.hcp_creds.data["client_secret"])
 }
