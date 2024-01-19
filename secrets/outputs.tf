eab3720d063a83c28b9a2d2755cb995fb96683c2
Lara, Benjamin R <ben.lara@hashicorp.com>
2024-01-19 14:14:52 -0500

    fix: :bug: update tf version and remove nonsensitive() calls

--- secrets/outputs.tf
+++ secrets/outputs.tf
@@ -1,15 +1,15 @@
 output "hcp_client_id" {
-  value = nonsensitive(data.vault_kv_secret_v2.hashistack.data["hcp_client_id"])
+  value = data.vault_kv_secret_v2.hashistack.data["hcp_client_id"]
 }
 
 output "hcp_client_secret" {
-  value = nonsensitive(data.vault_kv_secret_v2.hashistack.data["hcp_client_secret"])
+  value = data.vault_kv_secret_v2.hashistack.data["hcp_client_secret"]
 }
 
 output "tfe_token" {
-  value = nonsensitive(data.vault_kv_secret_v2.hashistack.data["tfe_token"])
+  value = data.vault_kv_secret_v2.hashistack.data["tfe_token"]
 }
 
 output "boundary_admin_password" {
-  value = nonsensitive(data.vault_kv_secret_v2.hashistack.data["boundary_admin_password"])
+  value = data.vault_kv_secret_v2.hashistack.data["boundary_admin_password"]
 }
