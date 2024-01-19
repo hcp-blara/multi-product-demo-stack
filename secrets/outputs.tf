2d112d6eb5420da3b2c5cc4d560b48571f0ab651
Lara, Benjamin R <ben.lara@hashicorp.com>
2024-01-19 14:59:21 -0500

    fix: :bug: add sensitive declaration to secret outputs

--- secrets/outputs.tf
+++ secrets/outputs.tf
@@ -1,15 +1,19 @@
 output "hcp_client_id" {
   value = data.vault_kv_secret_v2.hashistack.data["hcp_client_id"]
+  sensitive = true
 }
 
 output "hcp_client_secret" {
   value = data.vault_kv_secret_v2.hashistack.data["hcp_client_secret"]
+  sensitive = true
 }
 
 output "tfe_token" {
   value = data.vault_kv_secret_v2.hashistack.data["tfe_token"]
+  sensitive = true
 }
 
 output "boundary_admin_password" {
   value = data.vault_kv_secret_v2.hashistack.data["boundary_admin_password"]
+  sensitive = true
 }
